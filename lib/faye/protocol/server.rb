bdeacb8820eae16875a8a060bae74e9fd3bc06f3
stopdropandrew <stopdropandrew@gmail.com>
2015-03-12 12:18:22 -0700

    Pass advice/data added in Extensions into response

--- lib/faye/protocol/server.rb
+++ lib/faye/protocol/server.rb
@@ -76,6 +76,8 @@ module Faye
       response['clientId'] = message['clientId'] if message['clientId']
       response['channel']  = message['channel']  if message['channel']
       response['error']    = message['error']    if message['error']
+      response['advice']   = message['advice']   if message['advice']
+      response['data']     = message['data']     if message['data']
 
       response['successful'] = !response['error']
       response
